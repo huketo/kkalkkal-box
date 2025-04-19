e2d9620261edbefcafb78d28759cbfa4c31707a7
huketo <naruto1458@naver.com>
2025-04-19 20:56:40 +0900

    chore: :wrench: update ignore files and package.json for Nunjucks templates

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,12 @@ module.exports = {
     'prettier/prettier': 'error',
     'no-console': 'warn',
     'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
-    'node/no-unsupported-features/es-syntax': ['error', { ignores: ['modules'] }],
+    'node/no-unsupported-features/es-syntax': [
+      'error',
+      {
+        version: '>=14.0.0',
+        ignores: ['modules'],
+      },
+    ],
   },
 };
